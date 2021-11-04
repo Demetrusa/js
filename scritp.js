19c383407e0c7d3561ee227ea55ceb5af10aae35
Demetrusa <demetre.kurdadze20b@geolab.edu.gegit config --global user.name Demetrusacd /Desktop/Users/Git/Demegit config --global user.email demetre.kurdadze20b@geolab.edu.ge>
2021-11-04 16:30:53 +0400

    3

--- scritp.js
+++ scritp.js
@@ -1,8 +1,13 @@
 
-// let arr = [[2, -3, 5, 11], [1, -30, -11, 100], [-1, -3, -4]];
-//  for(let i = 0; i < 0; i++){
-//         console.log(arr[i])
-// }
+let arr = [[2, -3, 5, 11], [1, -30, -11, 100], [-1, -3, -4]];
+
+for (let i=0; i < arr.length; i++) {
+    for (let j=0; j < arr[i].length; j++) {
+      console.log(arr[i][j]);
+    }
+  }
+
+
 
 
 // let arr = [2, -3, 5, 11, 1, -30, -11, 100, -1, -3, -4];
@@ -11,13 +16,13 @@
 // }
 
 
- let array = ['javascript', 'php', 'c+', 'python', 'c++', 'kotlin', 'c', 'swift'];
-    for(let i = 0; i <= array.length; i++){
-        for(let a = 5; a <= array[i].length; a++) {
-            console.log(array[i])
-        }
+//  let array = ['javascript', 'php', 'c+', 'python', 'c++', 'kotlin', 'c', 'swift'];
+//     for(let i = 0; i <= array.length; i++){
+//         for(let a = 5; a <= array[i].length; a++) {
+//             console.log(array[i])
+//         }
     
-    }
+//     }
 
 
      
