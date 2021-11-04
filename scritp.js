c9723e2b789c496c2d1b2f2918ad7ca04421e6f9
Demetrusa <demetre.kurdadze20b@geolab.edu.gegit config --global user.name Demetrusacd /Desktop/Users/Git/Demegit config --global user.email demetre.kurdadze20b@geolab.edu.ge>
2021-11-04 15:58:55 +0400

    example

---
+++ scritp.js
@@ -0,0 +1,25 @@
+
+// let arr = [[2, -3, 5, 11], [1, -30, -11, 100], [-1, -3, -4]];
+//  for(let i = 0; i < 0; i++){
+//         console.log(arr[i])
+// }
+
+
+// let arr = [2, -3, 5, 11, 1, -30, -11, 100, -1, -3, -4];
+//     for (let i = 0; i >= 0; i++) {
+//     arr[i] = i;
+// }
+
+
+ let array = ['javascript', 'php', 'c+', 'python', 'c++', 'kotlin', 'c', 'swift'];
+    for(let i = 0; i <= array.length; i++){
+        for(let a = 5; a <= array[i].length; a++) {
+            console.log(array[i])
+        }
+    
+    }
+
+
+     
+
+// console.log(arr.fill[i])
